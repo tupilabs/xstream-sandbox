18b97bb7d41730f927dc42f87809a2b544956d67
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2013-04-06 13:19:10 -0300

    Tests with serialization

--- src/main/java/com/tupilabs/test/xstream/A.java
+++ src/main/java/com/tupilabs/test/xstream/A.java
@@ -1,5 +1,12 @@
 package com.tupilabs.test.xstream;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
 
 import com.thoughtworks.xstream.XStream;
@@ -9,9 +16,26 @@ public class A implements Serializable {
 	/*
 	 * Change for XStream conversion tests. 
 	 */
-	private static final long serialVersionUID = 2L;
+	private static final long serialVersionUID = 3L;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+        InputStream bis = new FileInputStream(new File("/tmp/test.s"));
+        ObjectInputStream is = new ObjectInputStream(bis);
+        A a = (A) is.readObject();
+        is.close();
+    }
+	
+	public static void main0(String[] args) throws Exception {
+        A a = new A();
+        OutputStream bos = new FileOutputStream(new File("/tmp/test.s"));
+        ObjectOutputStream os = new ObjectOutputStream(bos);
+        os.writeObject(a);
+        os.flush();
+        os.close();
+        System.out.println(bos.toString());
+    }
+	
+	public static void main2(String[] args) {
 		XStream xstream = new XStream();
 		String xml = "<com.tupilabs.test.xstream.A/>";
 		System.out.println((A) xstream.fromXML(xml));
